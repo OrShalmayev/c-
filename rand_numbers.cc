ba179785fd3cc043b4ac0b5389a5b610fb8d4485
classuser2 <classuser2@HQ.Taasuka.gov.il>
2019-11-04 12:56:03 +0200

    rand function

---
+++ rand_numbers.cc
@@ -0,0 +1,15 @@
+#include <iosream>
+#include <cstdlib>
+using namespace std;
+
+// Functions
+
+// Main function
+int main(){
+    // When used, we are required to include the header <cstdlib>.
+    cout<<rand();
+
+
+    // because the data type of the function is integer
+    return 0;
+}
